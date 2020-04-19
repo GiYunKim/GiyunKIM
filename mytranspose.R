830fd0f58fab781a8085f8dd8fdf93fb7f4078e7
GiYunKim <63787493+GiYunKim@users.noreply.github.com>
2020-04-19 19:05:48 +0900

    mytranspose.R

---
+++ mytranspose.R
@@ -0,0 +1,31 @@
+
+mytranspose <- function(x) {
+  if(is.null(x)){
+    return(x)
+  }
+  else if(all(is.na(x))){
+    return(NA)
+  }
+  else{
+  a <- as.matrix(x)
+  
+  y <- matrix(1, nrow=ncol(a), ncol = nrow(a))
+  for(i in 0:nrow(a)) {
+    for(j in 0:ncol(a)) {
+      y[j,i] <- a[i,j]
+    }
+  }
+  if(is.matrix(x)){
+      return(y)
+  }
+  else if(is.vector(x)){
+      return(cbind(x))
+    }
+  else if(is.data.frame(x)){
+      return(as.data.frame(y))
+    }
+  }
+}
+
+
+
